801674db4350f20f3f683e8dff805e696e1f8ec2
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2025-01-30 05:33:04 +0330

    Add title for HomeScreen

--- app/src/main/java/com/example/bazaaro/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/bazaaro/presentation/home/HomeScreen.kt
@@ -1,21 +1,35 @@
 package com.example.bazaaro.presentation.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.bazaaro.R
 import com.example.bazaaro.app.DETAIL_SCREEN_ROUTE
 import com.example.bazaaro.app.ui.components.ErrorView
 import com.example.bazaaro.app.ui.components.LoadingView
@@ -35,7 +49,8 @@ fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel(), navController: Na
         HomeState.Loading -> LoadingView()
         is HomeState.Success -> MainView(state.productList, onRefresh = {
             homeViewModel.getAllProducts()
-
+        }, goToFavorite = {
+            navController.navigate("favorite")
         }) {
             navController.navigate("$DETAIL_SCREEN_ROUTE/$it")
         }
@@ -48,6 +63,7 @@ private fun MainView(
     productList: List<Product>,
     isRefreshing: Boolean = false,
     onRefresh: () -> Unit,
+    goToFavorite: () -> Unit,
     clickHandler: (Int) -> Unit
 ) {
 
@@ -55,8 +71,41 @@ private fun MainView(
         Text(text = "Nothing to show")
     } else {
 
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.favorite))
+
         PullToRefreshBox(isRefreshing = isRefreshing, onRefresh = onRefresh) {
             LazyColumn {
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row(verticalAlignment = Alignment.Bottom) {
+
+                            Text(
+                                text = "B",
+                                fontWeight = FontWeight.ExtraBold,
+                                fontSize = 24.sp,
+                                color = Color(0xFFDB3022),
+                                fontFamily = FontFamily.Serif
+                            )
+                            Text(text = "azaaro", fontFamily = FontFamily.Monospace)
+
+                        }
+
+                        LottieAnimation(composition = composition,
+                            isPlaying = true,
+                            iterations = LottieConstants.IterateForever,
+                            modifier = Modifier
+                                .size(48.dp)
+                                .clickable {
+                                    goToFavorite()
+                                })
+                    }
+                }
                 items(productList.chunked(2)) {
                     Spacer(modifier = Modifier.height(24.dp))
                     Row(
